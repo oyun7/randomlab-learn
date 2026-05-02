c7034bba34795d8701b67f4678b78bb293de8fbb
oyun7 <oyuntyva07@gmail.com>
2026-05-03 00:01:12 +0700

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/randomlab-learn/',  // ← имя твоего репозитория на GitHub
+  base: '/',  // ← имя твоего репозитория на GitHub
 })
